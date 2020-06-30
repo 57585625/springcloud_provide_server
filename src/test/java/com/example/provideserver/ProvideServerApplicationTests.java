0a70df97704597358794bfe6885c6e90cbdcfa48
zy57585625 <623653585@qq.com>
2020-06-30 11:35:52 +0800

    springcloud_private 服务提供者（生产者），该项目的配置文件交由config注册中心管理了。

---
+++ src/test/java/com/example/provideserver/ProvideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provideserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvideServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
